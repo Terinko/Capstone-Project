b6721db026c60d643150296b8b5e47f06da9423f
Tyler <rinkotyler@gmail.com>
2025-11-19 20:30:21 -0500

    Changing the login/create account pages to modals

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
+import "./App.css";
 import App from "./App.tsx";
-import Login from "./login.tsx";
-import CreateAccount from "./createAcct.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")!).render(
@@ -12,8 +11,6 @@ createRoot(document.getElementById("root")!).render(
     <Routes>
       <Route path="/">
         <Route index element={<App />}></Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/createaccount" element={<CreateAccount />}></Route>
       </Route>
     </Routes>
   </BrowserRouter>
